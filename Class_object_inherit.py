e34c64bd75f368125f9ff6288f4896315f119a4d
LuHu <luhu0105@gmail.com>
2017-12-15 09:36:39 +0800

    登录模块化以及为什么新类要继承object类的原因

---
+++ Class_object_inherit.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# __author__=luhu
+"""
+        继承 object 类的是新式类，不继承 object 类的是经典类，在 Python 2.7 里面新式类和经典
+    类在多继承方面会有差异,B、C 是 A 的子类，D 多继承了 B、C 两个类，其中 C 重写了 A 中的
+    foo() 方法。如果 A 是经典类（如下代码），当调用 D 的实例的 foo() 方法时，Python 会按照深
+    度优先的方法去搜索 foo() ，路径是 B-A-C ，执行的是 A 中的 foo() ；如果 A 是新式类，当调用
+    D 的实例的 foo() 方法时，Python 会按照广度优先的方法去搜索 foo() ，路径是 B-C-A ，执行的
+    是 C 中的 foo() 。因为 D 是直接继承 C 的，从逻辑上说，执行 C 中的 foo() 更加合理，因此新
+    式类对多继承的处理更为合乎逻辑。在 Python 3.x 中的新式类貌似已经兼容了经典类，无论 A 是否
+    继承 object 类， D 实例中的 foo() 都会执行 C 中的 foo() 。但是在 Python 2.7 中这种差异仍然
+    存在，因此还是推荐使用新式类，要继承 object 类。
+
+"""
+
+
+class A:
+    def foo(self):
+        print('called A.foo()')
+
+
+class B(A):
+    pass
+
+
+class C(A):
+    def foo(self):
+        print('called C.foo()')
+
+
+class D(B, C):
+    pass
+
+
+if __name__ == '__main__':
+    d = D()
+    d.foo()
--- login_wangyi.py
+++ login_wangyi.py
@@ -12,10 +12,28 @@ driver = webdriver.Chrome()
 driver.get('http://www.jstjjg.com/manage/')
 driver.implicitly_wait(10)
 
+"""
+        创建表Account类，对用户名密码进行初始化设置，紧接着创建login_as()函数用于实现用户的
+    登录操作，他需要一个user_info参数用于接收用户的登录信息，取user_name中的username输入到用户名
+    输入框中，取user_info中的password输入密码输入框。
+        紧接着通过调用Account实例化用户admin，进行个性化的参数设置，最后调用login_as()函数来
+    实现用户的登录。  
+        创建Account类，继承object类，2.x 里 object 是一个较新基类，在3.x里object已经做为所有
+    东西的基类。
+        继承 object 类的是新式类，不继承 object 类的是经典类，在 Python 2.7 里面新式类和经典类
+    在多继承方面会有差异，新式类对多继承的处理更为合乎逻辑。
+"""
 
-def login():
-    driver.find_element_by_name('username').send_keys('portal')
-    driver.find_element_by_name('password').send_keys('portal123')
+
+class Account(object):      # 在Python3.x中可以不继承object类，在3.x里object已经做为所有东西的基类
+    def __init__(self, username='', password=''):
+        self.username = username
+        self.password = password
+
+
+def login_as(user_info):
+    driver.find_element_by_name('username').send_keys(user_info.username)
+    driver.find_element_by_name('password').send_keys(user_info.password)
     driver.find_element_by_id('btn_login').click()
 
 
@@ -28,6 +46,7 @@ def logout():
     driver.close()
 
 
-login()
+admin = Account(username='portal', password='portal123')
+login_as(admin)
 sleep(3)
 logout()
