f2df76bc3e8d66e847d46e994882bfaa6a47ccf1
LuHu <luhu0105@gmail.com>
2017-12-18 16:47:31 +0800

    添加火狐浏览器调用方法

--- my_selenium/selenium_init.py
+++ my_selenium/selenium_init.py
@@ -10,3 +10,12 @@ driver.find_element_by_id('kw').send_keys('selenium2')
 driver.find_element_by_id('su').click()
 sleep(5)
 driver.quit()
+
+
+# driver2 = webdriver.Firefox()
+# fp = webdriver.FirefoxProfile('C:\Users\Administrator\AppData\Roaming\Mozilla\Firefox\Profiles\qp6zygus.default')
+browser = webdriver.Firefox()
+browser.get("https://www.baidu.com")
+browser.find_element_by_id('kw').send_keys('selenium2')
+browser.find_element_by_id('su').click()
+browser.close()
