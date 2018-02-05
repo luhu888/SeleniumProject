78c34a2d3aeeeeec031d7c058c86c33eaf85cfbc
LuHu <luhu0105@gmail.com>
2018-02-05 15:23:55 +0800

    add example

--- base/my_python_example.py
+++ base/my_python_example.py
@@ -62,16 +62,122 @@ def my_judge_prime_number():
         print(num, "不是质数")
 
 
+def my_exchange1():      # 使用临时变量进行变量交换
+    x = input('输入x的值：')
+    y = input('输入y的值：')
+    temp = x
+    x = y
+    y = temp
+    print(x, y)
+
+
+def my_exchange2():       # 不使用临时变量直接进行变量交换
+    x = input('输入x的值：')
+    y = input('输入y的值：')
+    x, y = y, x
+    print(x, y)
+
+
+"""
+    如果一个n位正整数等于其各位数字的n次方之和,则称该数为阿姆斯特朗数。 例如1^3 + 5^3 + 3^3 = 153。
+    1000以内的阿姆斯特朗数： 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407。
+"""
+
+
+def my_check_armstrong():
+    # Python 检测用户输入的数字是否为阿姆斯特朗数
+
+    # 获取用户输入的数字
+    num = int(input("请输入一个数字: "))
+
+    # 初始化变量 sum
+    sum = 0
+    # 指数
+    n = len(str(num))
+
+    # 检测
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** n
+        temp //= 10
+
+    # 输出结果
+    if num == sum:
+        print(num, "是阿姆斯特朗数")
+    else:
+        print(num, "不是阿姆斯特朗数")
+
+
+def my_sys_convert():
+    dec = int(input("输入数字："))
+
+    print("十进制数为：", dec)
+    print("转换为二进制为：", bin(dec))
+    print("转换为八进制为：", oct(dec))
+    print("转换为十六进制为：", hex(dec))
+
+
+def my_asc2_character():
+    # 用户输入字符
+    c = input("请输入一个字符: ")
+    # 用户输入ASCII码，并将输入的数字转为整型
+    a = int(input("请输入一个ASCII码: "))
+    print(c + " 的ASCII 码为", ord(c))
+    print(a, " 对应的字符为", chr(a))
+
+
+"""
+    欧几里得算法：
+    两个数的最大公约数可以使用 欧几里得算法实现。即两个数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数。
+"""
+
+
+def my_gcd():        # greatest common divisor    最大公约数
+    n1 = int(input('请输入一个正整数：'))
+    n2 = int(input('请输入另一个正整数：'))
+    if n1 == 0:
+        n1, n2 = n2, n1
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+    return n1
+
+
+def my_lcm():
+    n1 = int(input('请输入一个正整数：'))
+    n2 = int(input('请输入另一个正整数：'))
+    temp1 = n1
+    temp2 = n2
+    if n1 == 0:
+        n1, n2 = n2, n1
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+    return int(temp1 * temp2 / n1)
+
+
+def my_calendar():
+    # 引入日历模块
+    import calendar
+
+    # 输入指定年月
+    yy = int(input("输入年份: "))
+    mm = int(input("输入月份: "))
+
+    # 显示日历
+    print(calendar.month(yy, mm))
+
+
 if __name__ == '__main__':
     # my_square_root()
     # my_square_root2()
     # my_square_triangle()
     # my_judge_leap_year()
-    my_judge_prime_number()
-
-
-
-
+    # my_judge_prime_number()
+    # my_exchange2()
+    # my_exchange1()
+    # my_asc2_character()
+    # print(my_gcd())
+    print(my_lcm())
 
 
 
